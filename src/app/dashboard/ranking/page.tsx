fc01a3e630bd38232e958a1226a44e9140f1330f
Paulo Thiago S. de F. <thiago25pro@gmail.com>
2023-12-21 00:35:02 -0300

    ranking mobile is now equal to desktop

--- src/app/dashboard/ranking/page.tsx
+++ src/app/dashboard/ranking/page.tsx
@@ -8,42 +8,6 @@ import Image from "next/image";
 import { useEffect, useState } from "react";
 
 const Ranking = () => {
-  const [screenSize, setScreenSize] = useState({
-    width: getCurrentDimension() as unknown as number | 0,
-  });
-
-  const [isClient, setIsClient] = useState(false);
-
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
-
-  useEffect(() => {
-    if (isClient) {
-      const updateDimension = () => {
-        const dimensions = getCurrentDimension();
-        setScreenSize(dimensions);
-      };
-      window.addEventListener("resize", updateDimension);
-      return () => {
-        window.removeEventListener("resize", updateDimension);
-      };
-    }
-  }, []);
-
-  useEffect(() => {
-    if (isClient) {
-      const updateDimension = () => {
-        const dimensions = getCurrentDimension();
-        setScreenSize(dimensions);
-      };
-      window.addEventListener("resize", updateDimension);
-      return () => {
-        window.removeEventListener("resize", updateDimension);
-      };
-    }
-  }, [isClient, screenSize]);
-
   const { getRanking } = useService();
 
   const { data } = useQuery({
@@ -51,127 +15,32 @@ const Ranking = () => {
     queryFn: getRanking,
   });
 
-  if (isClient) {
-    return (
-      <main>
-        {data && (
-          <>
-            <Podium data={data} screenSize={screenSize} />
-            <RankingGrid data={data} screenSize={screenSize} />
-          </>
-        )}
-      </main>
-    );
-  }
+  return (
+    <main>
+      {data && (
+        <>
+          <Podium data={data} />
+          <RankingGrid data={data} />
+        </>
+      )}
+    </main>
+  );
 };
 
 export default Ranking;
 
 interface PodiumProps {
   data: RankingUserType[];
-  screenSize: {
-    width: number;
-  };
 }
 
 interface RankingGridProps extends PodiumProps {}
 
-const Card = ({
-  user,
-  i,
-  screenSize,
-}: {
-  user: RankingUserType;
-  i: number;
-  screenSize: { width: number };
-}) => {
-  if (screenSize && screenSize.width < 640) {
-    return (
-      <div className={`shadow-2xl shadow-sky-900 rounded-md p-4`}>
-        <div className="flex justify-between mb-8 border-b-2 pb-4">
-          {(() => {
-            if (i === 0 || i === 1 || i === 2) {
-              return (
-                <>
-                  <div className="font-semibold">
-                    <span className="bg-black text-white p-2 text-sm rounded-full mr-2">
-                      {i === 0 && "#1"}
-                      {i === 1 && "#2"}
-                      {i === 2 && "#3"}
-                    </span>
-                    {i === 0 && "Primeiro lugar"}
-                    {i === 1 && "Segundo lugar"}
-                    {i === 2 && "Terceiro lugar"}
-                  </div>
-                  {i === 0 && (
-                    <Image
-                      src={"/1st.png"}
-                      alt="posição"
-                      width={20}
-                      height={20}
-                    />
-                  )}
-                  {i === 1 && (
-                    <Image
-                      src={"/2nd.png"}
-                      alt="posição"
-                      width={20}
-                      height={20}
-                    />
-                  )}
-                  {i === 2 && (
-                    <Image
-                      src={"/3rd.png"}
-                      alt="posição"
-                      width={20}
-                      height={20}
-                    />
-                  )}
-                </>
-              );
-            }
-          })()}
-        </div>
-        <div className="mb-2 w-full flex">
-          <div>
-            <AvatarByName
-              color="#000"
-              fgColor="#fff"
-              size="40"
-              className="rounded-full mr-2"
-              name={user?.name}
-              maxInitials={2}
-            />
-          </div>
-
-          <div className="flex flex-col ml-4">
-            <span className="">{user.name}</span>
-            <span className="text-black font-bold">
-              {user.name.split(" ")[1]}
-            </span>
-          </div>
-        </div>
-        <div className="flex justify-between">
-          <div className="flex flex-col">
-            <span className="text-black">{user.votes}</span>
-            <span className="text-black font-bold">votos</span>
-          </div>
-          <div className="flex flex-col">
-            <span className="text-black">{user.rating.name}</span>
-            <span className="text-black font-bold">
-              {user.rating.description}
-            </span>
-          </div>
-        </div>
-      </div>
-    );
-  }
-
+const Card = ({ user, i }: { user: RankingUserType; i: number }) => {
   return (
     <div
       className={`${i === 0 ? "mt-16" : ""} ${
         i === 2 ? "mt-32" : ""
-      } shadow-2xl shadow-sky-900 rounded-md p-4`}
+      } shadow-2xl shadow-sky-900 rounded-md p-2 md:p-4`}
     >
       <div className="flex justify-between mb-8 border-b-2 pb-4">
         {(() => {
@@ -252,7 +121,7 @@ const Card = ({
   );
 };
 
-const Podium = ({ data, screenSize }: PodiumProps) => {
+const Podium = ({ data }: PodiumProps) => {
   const [tops, setTops] = useState<RankingUserType[]>([]);
   const [hasUpdated, setHasUpdated] = useState(false);
 
@@ -273,49 +142,28 @@ const Podium = ({ data, screenSize }: PodiumProps) => {
     }
   }, [data, hasUpdated]);
 
-  if (screenSize && screenSize.width < 640) {
-    return (
-      <>
-        <h4 className="text-black font-semibold mx-auto rounded-lg bg-white w-fit px-4 py-2">
-          Ranking
-        </h4>
-        <div className="grid grid-cols-1 gap-8 px-4 py-2 my-8">
-          {data.map((user, i) => (
-            <Card key={i} i={i} user={user} screenSize={screenSize} />
-          ))}
-        </div>
-      </>
-    );
-  }
-
   return (
     <>
       <h4 className="text-black font-semibold mx-auto rounded-lg bg-white w-fit px-4 py-2">
         Ranking
       </h4>
-      <div className="flex gap-4 items-start justify-center md:gap-8 py-2 my-8">
+      <div className="flex gap-2 p-2 items-start justify-center md:gap-8 py-2 my-8">
         {tops.map((user, i) => (
-          <Card key={i} i={i} user={user} screenSize={screenSize} />
+          <Card key={i} i={i} user={user} />
         ))}
       </div>
     </>
   );
 };
 
-const RankingGrid = ({ data, screenSize }: RankingGridProps) => {
+const RankingGrid = ({ data }: RankingGridProps) => {
   return (
     <div className="md:grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 px-4 py-2 border-t-2 my-8">
       {data.map((user, i) => {
         if (i > 2) {
-          return <Card key={i} user={user} i={i} screenSize={screenSize} />;
+          return <Card key={i} user={user} i={i} />;
         }
       })}
     </div>
   );
 };
-
-function getCurrentDimension() {
-  return {
-    width: window.innerWidth,
-  };
-}
