00d0cb22efdf6d590b8dcd2c47d237cb935a8f7a
Paulo Thiago S. de F. <thiago25pro@gmail.com>
2023-12-22 17:34:13 -0300

    remove comment

--- src/components/Podium/index.tsx
+++ src/components/Podium/index.tsx
@@ -6,43 +6,6 @@ export interface PodiumProps {
 }
 
 export const Podium = ({ data }: PodiumProps) => {
-  //   const rearrangeRanking = (data: RankingUserType[]) => {
-  //     const newTops = data.slice(0, 3).map((user, index) => {
-  //       if (index === 0) {
-  //         const aux = data[index];
-  //         data[index] = data[1];
-  //         data[1] = aux;
-  //       }
-  //       return user;
-  //     });
-  //     return newTops;
-  //   };
-
-  //   const rearrangeRanking = (data: RankingUserType[]) => {
-  //     const newTops = data.slice(0, 3).map((user, index) => {
-  //       if (index < 2) {
-  //         const aux = data[index];
-  //         data[index] = data[index + 1];
-  //         data[index + 1] = aux;
-  //       }
-  //       return user;
-  //     });
-
-  //     return newTops;
-  //   };
-
-  //   const rearrangeRanking = (data: RankingUserType[]) => {
-  //     if (data.length >= 3) {
-  //       const aux = data[0];
-  //       data[0] = data[1];
-  //       data[1] = aux;
-  //     }
-
-  //     return data.slice(0, 3);
-  //   };
-
-  //   const tops = rearrangeRanking(data);
-
   return (
     <>
       <h4 className="text-black font-semibold mx-auto rounded-lg bg-white w-fit px-4 py-2">
